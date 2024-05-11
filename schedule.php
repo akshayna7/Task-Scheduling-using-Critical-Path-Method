bc390604f6757bd0be79b80e1325230f14b0237b
Akshay Nair A <akshay.a@buildnext.in>
2024-05-11 12:20:25 +0530

    dependency type implementation

--- schedule.php
+++ schedule.php
@@ -1,7 +1,7 @@
 <?php
-//Example case
+//Example input
 $tasks = [
-    [
+    [ 
         'id' => '1',
         'name' => 'A',
         'duration' => '2',
@@ -20,7 +20,7 @@ $tasks = [
         'name' => 'B',
         'duration' => '4',
         'dependencies' => ['1'],
-        'relationship' => ['sf'],
+        'relationship' => ['ss'],
         'offset' => [0],
         'ES' => 0,
         'EF' => 0,
@@ -33,8 +33,8 @@ $tasks = [
         'id' => '3',
         'name' => 'C',
         'duration' => '10',
-        'dependencies' => ['2'],
-        'relationship' => ['sf'],
+        'dependencies' => ['1'],
+        'relationship' => ['ss'],
         'offset' => [0],
         'ES' => 0,
         'EF' => 0,
@@ -47,37 +47,9 @@ $tasks = [
         'id' => '4',
         'name' => 'D',
         'duration' => '1',
-        'dependencies' => ['2'],
-        'relationship' => ['sf'],
-        'offset' => [0],
-        'ES' => 0,
-        'EF' => 0,
-        'LS' => 0,
-        'LF' => 0,
-        'float' => 0,
-        'isCritical' => false,
-    ],
-    [
-        'id' => '5',
-        'name' => 'E',
-        'duration' => '7',
-        'dependencies' => ['4'],
-        'relationship' => ['sf'],
-        'offset' => [0],
-        'ES' => 0,
-        'EF' => 0,
-        'LS' => 0,
-        'LF' => 0,
-        'float' => 0,
-        'isCritical' => false,
-    ],
-    [
-        'id' => '6',
-        'name' => 'F',
-        'duration' => '15',
-        'dependencies' => ['4'],
-        'relationship' => ['sf'],
-        'offset' => [0],
+        'dependencies' => ['2','3'],
+        'relationship' => ['ss','ss'],
+        'offset' => [0,0],
         'ES' => 0,
         'EF' => 0,
         'LS' => 0,
@@ -85,7 +57,7 @@ $tasks = [
         'float' => 0,
         'isCritical' => false,
     ]
-];
+    ];
 
 
 $taskIdToIndex = [];
@@ -97,11 +69,13 @@ foreach ($tasks as $index => $task) {
 
 // FORWARD PASS
 foreach ($tasks as $key => &$task) {
+
     if (empty($task['dependencies'])) {
-        $task['ES'] = 1;
+        $task['ES'] = 0;
         $task['EF'] = $task['duration'];
     } else {
         $maxEF = 0;
+        $maxES = 0;
         foreach ($task['dependencies'] as $k => $dependencyId) {
             $relationship = $task['relationship'][$k];
             $offset = $task['offset'][$k];
@@ -109,69 +83,94 @@ foreach ($tasks as $key => &$task) {
 
             switch ($relationship) {
                 case 'ss': // Start-to-Start
-                    $maxEF = max($maxEF, $dependency['ES'] + $offset);
+                    $maxES = max($maxES, $dependency['ES'] + $offset);
+                    $task['ES'] = $maxES;
+                    $task['EF'] = $maxEF = $task['ES'] + $task['duration'];
                     break;
                 case 'sf': // Start-to-Finish
-                    $maxEF = max($maxEF, $dependency['EF'] + $offset);
+                    $maxEF = max($maxEF, $dependency['ES'] + $offset);
+                    $task['EF'] = $maxEF;
+                    $task['ES'] = $maxES = $task['EF'] - $task['duration'];
                     break;
                 case 'ff': // Finish-to-Finish
-                    $maxEF = max($maxEF, $dependency['LF'] + $offset);
+                    $maxEF = max($maxEF, $dependency['EF'] + $offset);
+                    $task['EF'] = $maxEF;
+                    $task['ES'] = $maxES = $task['EF'] - $task['duration'];
+
                     break;
                 case 'fs': // Finish-to-Start
-                    $maxEF = max($maxEF, $dependency['LS'] + $offset);
+                    $maxES = max($maxES, $dependency['EF'] + $offset);
+                    $task['ES'] = $maxES;
+                    $task['EF'] = $maxEF = $task['ES'] + $task['duration'];
                     break;
             }
 
             //$maxEF = max($maxEF, $dependency['EF']);
-            $tasks[$taskIdToIndex[$dependencyId]]['successors'][]=$task['id'];
+            $tasks[$taskIdToIndex[$dependencyId]]['successors'][]= array('id'=>$task['id'],'key'=>$k);
         }
-        $task['ES'] = $maxEF + 1;
-        $task['EF'] = $task['ES'] + $task['duration'] - 1;
     }
+
     $maxPS = max($maxPS,$task['EF']);
 }
+
+
+
 
 
 // BACKWARD PASS
 $lastTaskIndex = count($tasks) - 1;
 $lastTask = &$tasks[$lastTaskIndex];
-$lastTask['LF'] = $lastTask['EF'];
+$lastTask['LF'] = $maxPS;
 $lastTask['LS'] = $lastTask['ES']; // Set LS based on ES for the last task
-
+$task['float'] = $lastTask['LF'] - $lastTask['EF'];
+$task['isCritical'] = ((int)$task['float'] == 0); 
 for ($i = $lastTaskIndex; $i >= 0; $i--) {
+
   $task = &$tasks[$i];
   if (empty($task['successors'])) {
+
     $task['LF'] = $maxPS;
-    $task['LS'] = $task['LF'] - $task['duration'] + 1;
+    $task['LS'] = $task['LF'] - $task['duration'];
+
+  // echo print_r($task);
   } else {
-    $minEF = PHP_INT_MAX;
-    foreach ($task['successors'] as $k => $successorId) {
+    $minLF = PHP_INT_MAX;
+    $minLS = PHP_INT_MAX;
+    foreach ($task['successors'] as $k => $successorData) {
+      $successorId = $successorData['id']; 
+      $keyIndex = $successorData['key']; 
       $successor = $tasks[$taskIdToIndex[$successorId]];
+      $relationship = $successor['relationship'][$keyIndex];
+      $offset = $successor['offset'][$keyIndex];
 
-      //$relationship = $successor['relationship'][$k];
-      //$offset = $successor['offset'][$k];
-
-      // switch ($relationship) {
-      //           case 'ss': // Start-to-Start
-      //               $minEF = min($minEF, $successor['LS'] + $offset);
-      //               break;
-      //           case 'sf': // Start-to-Finish
-      //               $minEF = min($minEF, $successor['EF'] + $offset);
-      //               break;
-      //           case 'ff': // Finish-to-Finish
-      //               $minEF = min($minEF, $successor['LF'] + $offset);
-      //               break;
-      //           case 'fs': // Finish-to-Start
-      //               $minEF = min($minEF, $successor['LS'] + $offset);
-      //               break;
-      //   }
-      $minEF = min($minEF, $successor['LS']); // Use LF of predecessors
+      switch ($relationship) {
+                case 'ss': // Start-to-Start
+                    $minLS = min($minLS, $successor['LS'] - $offset);
+                    $task['LS'] = $minLS ;
+                    $task['LF'] = $minLF = $task['LS'] + $task['duration'];
+                    break;
+                case 'sf': // Start-to-Finish
+                    $minLS = min($minLF, $successor['LF'] - $offset);
+                    $task['LS'] = $minLS ;
+                    $task['LF'] = $minLF = $task['LS'] + $task['duration'];
+                    break;
+                case 'ff': // Finish-to-Finish
+                    $minLF = min($minLF, $successor['LF'] - $offset);
+                    $task['LF'] = $minLF;
+                    $task['LS'] = $minLS = $task['LF'] - $task['duration'];
+                    break;
+                case 'fs': // Finish-to-Start
+                    $minLF = min($minLF, $successor['LS'] - $offset);
+                    $task['LF'] = $minLF;
+                    $task['LS'] = $minLS = $task['LF'] - $task['duration'];
+                    break;
+        }
+      //$minEF = min($minEF, $successor['LS']); // Use LF of predecessors
     }
-    $task['LF'] = $minEF - 1; // Directly set LS based on minEF
-    $task['LS'] = $task['LF'] - $task['duration'] + 1;
   }
   $task['float'] = $task['LF'] - $task['EF'];
-  $task['isCritical'] = ($task['float'] == 0); // Adjust epsilon if needed
+  $task['isCritical'] = ((int)$task['float'] == 0); // Adjust epsilon if needed
+ 
 }
 
 // Set the last task as critical if its float is 0
@@ -184,10 +183,4 @@ foreach ($tasks as $task) {
     echo "{$task['id']}, {$task['name']}, {$task['duration']}, {$task['ES']}, {$task['EF']}, {$task['LS']}, {$task['LF']}, {$task['float']}, {$isCritical}\n";
 }
 
-
-
-
-
-
-
-
+?>
